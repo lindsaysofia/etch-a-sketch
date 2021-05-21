02481de59682931ca681331a4b24132a0a1bc420
Lindsay Lopez <lopez95@berkeley.edu>
2021-05-20 18:57:34 -0700

    fixed issue where 'cancel' didn't exit out of prompt

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const changeSizeButton = document.querySelector('button');
 const root = document.querySelector(':root');
 const size = 16;
 
-changeSizeButton.addEventListener('click', changeSize);
+changeSizeButton.addEventListener('click', changeGridSize);
 
 createGrid(size, container);
 
@@ -18,10 +18,11 @@ function createGrid(size, container) {
   root.style.setProperty('--size', size);
 }
 
-function changeSize() {
+function changeGridSize() {
   let newSize;
   do {
-    newSize = +prompt('Enter an integer between 1 and 100', 16);
+    newSize = prompt('Enter an integer between 1 and 100', 16);
+    newSize = (newSize === null) ? size : +newSize;
   } while (!Number.isInteger(newSize) || newSize <= 0 || newSize > 100);
   createGrid(newSize, container);
 }
